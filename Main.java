c442fc4c08cdb9b3680895ac1f6b0668cb1ff797
Karol <karolks94@gmail.com>
2018-12-13 19:44:16 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,20 @@
-import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
 
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws RecognitionException, IOException {
+    public static void main(String[] args) throws IOException {
 
-        boolean thrown = false;
 
         CharStream input = CharStreams.fromFileName(args[0]);
         YARSpgLexer lexer = new YARSpgLexer(input);
         YARSpgParser parser = new YARSpgParser(new CommonTokenStream(lexer));
         parser.yarspg();
-
-
+        
     }
 }
 
